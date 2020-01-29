f88cc83f3b5e4641b7a12e49e12f153602d4b721
Cassy Blaise <cassyblaise@gmail.com>
2020-01-29 19:15:36 +0100

    added a checkout button

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon';
+import CartDropdown from '../cart-dropdown/cart-dropdown';
 
 import './header.styles.scss';
 
@@ -27,7 +30,9 @@ const Header = ({currentUser}) =>(
               SIGN IN
             </Link>
           )}
+          <CartIcon />
        </div>
+       <CartDropdown />
     </div>
 );
 
